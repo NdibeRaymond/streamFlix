acffb8842d1211300ec35002148e668c435e52e7
Raymond Ndibe <ndiberaymond1@gmail.com>
2019-10-21 21:28:11 +0100

    third commit before deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDmW7txJques62mBJChwPt7JC8xBjRVaw4",
+    authDomain: "exemplary-range-237923.firebaseapp.com",
+    databaseURL: "https://exemplary-range-237923.firebaseio.com",
+    projectId: "exemplary-range-237923",
+    storageBucket: "exemplary-range-237923.appspot.com",
+    messagingSenderId: "836781910248",
+    appId: "1:836781910248:web:05f475786c127e9a96815d",
+    measurementId: "G-HLNRM3W89W"
+  }
 };
